a17bcc3fab566e85bc88eef785faecadc242be72
Dylan Curry <spicemasterD@gmail.com>
2025-05-19 20:13:32 +0300

    Incorporate test data into test

---
+++ tests/routers/router_test_data.py
@@ -0,0 +1,101 @@
+test_get_exercises_data = [
+    {
+        "id": 1,
+        "description": "Push-up exercise for chest and triceps.",
+        "reps": 15,
+        "sets": 2,
+        "exercise_name": "Push-Up",
+        "link": None,
+        "user_id": 1001
+    },
+    {
+        "id": 2,
+        "description": "Sit-ups for abdominal muscles.",
+        "reps": 20,
+        "sets": 2,
+        "exercise_name": "Sit-Up",
+        "link": "",
+        "user_id": 1001
+    },
+    {
+        "id": 3,
+        "description": "Pull-ups for back and biceps.",
+        "reps": 10,
+        "sets": 2,
+        "exercise_name": "Pull-Up",
+        "link": "https://www.youtube.com/watch?v=dAScZVF5o9k",
+        "user_id": 1001
+    },
+    {
+        "id": 4,
+        "description": "Squats for legs and glutes.",
+        "reps": 12,
+        "sets": 2,
+        "exercise_name": "Squat",
+        "link": "",
+        "user_id": 1001
+    },
+    {
+        "id": 6,
+        "description": "Bicep curls for arm strength.",
+        "reps": 15,
+        "sets": 2,
+        "exercise_name": "Bicep Curl",
+        "link": "",
+        "user_id": 1001
+    },
+    {
+        "id": 9,
+        "description": "Mountain climbers for cardio.",
+        "reps": 20,
+        "sets": 2,
+        "exercise_name": "Mountain Climber",
+        "link": "",
+        "user_id": 1001
+    },
+    {
+        "id": 11,
+        "description": "Deadlifts for back and leg strength.",
+        "reps": 8,
+        "sets": 2,
+        "exercise_name": "Deadlift",
+        "link": "",
+        "user_id": 1001
+    },
+    {
+        "id": 12,
+        "description": "Overhead press for shoulders.",
+        "reps": 12,
+        "sets": 2,
+        "exercise_name": "Overhead Press",
+        "link": "",
+        "user_id": 1001
+    },
+    {
+        "id": 14,
+        "description": "Step-ups for legs and cardio.",
+        "reps": 10,
+        "sets": 2,
+        "exercise_name": "Step-Up",
+        "link": "",
+        "user_id": 1001
+    },
+    {
+        "id": 15,
+        "description": "Russian twists for obliques.",
+        "reps": 20,
+        "sets": 2,
+        "exercise_name": "Russian Twist",
+        "link": "",
+        "user_id": 1001
+    },
+    {
+        "id": 18,
+        "description": "Bench press for chest.",
+        "reps": 10,
+        "sets": 2,
+        "exercise_name": "Bench Press",
+        "link": "",
+        "user_id": 1001
+    }
+]
--- tests/routers/test_routers.py
+++ tests/routers/test_routers.py
@@ -1,7 +1,11 @@
 import pytest
+import pdb
+import deepdiff
 from fastapi.testclient import TestClient
+from router_test_data import test_get_exercises_data
 
 def test_get_exercises(client: TestClient):
     response = client.get("/exercises")
     assert response.status_code == 200
-    print(response.json)
+    assert deepdiff.DeepDiff(response.json(), test_get_exercises_data, ignore_order=True) == {}
+
